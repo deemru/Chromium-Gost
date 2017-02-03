b3226dca47d0cdf1b31ac1b95a9a6c5a40c060fc
Dmitrii Pichulin <deem@deem.ru>
2017-02-03 15:37:43 +0300

    little batch fixes

--- build_windows/chromium-gost-prepare.bat
+++ build_windows/chromium-gost-prepare.bat
@@ -5,18 +5,18 @@ set DEPOT_TOOLS_WIN_TOOLCHAIN=0
 set GYP_MSVS_VERSION=2015
 
 cd %BORINGSSL_PATH%
-call git checkout master
+call git checkout -f master
 call git reset --hard
 
 cd %CHROMIUM_PATH%
 call git fetch --tags
 call git checkout -b GOSTSSL-%CHROMIUM_TAG% tags/%CHROMIUM_TAG%
-call git checkout GOSTSSL-%CHROMIUM_TAG%
+call git checkout -f GOSTSSL-%CHROMIUM_TAG%
 call gclient sync
-call git am --3way --ignore-space-change --keep-cr < %CHROMIUM_GOST_REPO%\patch\chromium.patch
+call git am --3way --ignore-space-change < %CHROMIUM_GOST_REPO%\patch\chromium.patch
 
 cd %BORINGSSL_PATH%
 call git checkout -b GOSTSSL-%CHROMIUM_TAG%
-call git checkout GOSTSSL-%CHROMIUM_TAG%
-call git am --3way --ignore-space-change --keep-cr < %CHROMIUM_GOST_REPO%\patch\boringssl.patch
+call git checkout -f GOSTSSL-%CHROMIUM_TAG%
+call git am --3way --ignore-space-change < %CHROMIUM_GOST_REPO%\patch\boringssl.patch
 timeout 60
