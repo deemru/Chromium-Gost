2501476cd05ef97b36258801bae80cf87d3e29f1
Dmitrii Pichulin <deem@deem.ru>
2017-02-13 18:56:50 +0300

    fix DATETIMEVERSION on eng locale

--- build_windows/chromium-gost-build-gostssl.bat
+++ build_windows/chromium-gost-build-gostssl.bat
@@ -2,7 +2,15 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
 cd /d %~dp0
 call chromium-gost-env.bat
 
-set DATETIMEVERSION=%DATE:~6,4%,%DATE:~3,2%,%DATE:~0,2%,%TIME:~0,2%%TIME:~3,2%
+set DATETIMEVERSION=%DATE:~3,1%
+if "%DATETIMEVERSION%" == " " (
+:: en-us
+    set DATETIMEVERSION=%DATE:~10,4%,%DATE:~4,2%,%DATE:~7,2%,%TIME:~0,2%%TIME:~3,2%
+) else (
+:: ru-ru
+    set DATETIMEVERSION=%DATE:~6,4%,%DATE:~3,2%,%DATE:~0,2%,%TIME:~0,2%%TIME:~3,2%
+)
+
 ( echo #define DATETIMEVERSION %DATETIMEVERSION%) > gostssl_ver.rc
 ( echo #define CHROMIUM_TAG "%CHROMIUM_TAG%") >> gostssl_ver.rc
 
