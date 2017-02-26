361f70dab2092f4b7bcc326325f5b6c10233186a
Dmitrii Pichulin <deem@deem.ru>
2017-02-26 14:38:42 +0300

    tuning ci build

--- build_windows/chromium-gost-publish-release.bat
+++ build_windows/chromium-gost-publish-release.bat
@@ -3,3 +3,5 @@ call chromium-gost-env.bat
 
 github-release release --user deemru --repo chromium-gost --tag %CHROMIUM_TAG% --draft
 github-release upload --user deemru --repo chromium-gost --tag %CHROMIUM_TAG% --name chromium-gost-%CHROMIUM_TAG%-win32.7z --file chromium-gost-%CHROMIUM_TAG%-win32.7z
+
+if "%1"=="" timeout 86400
