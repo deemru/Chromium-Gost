6b88cd36e5d92bfedc1622579d0726fcd1c8fd84
Dmitrii Pichulin <deem@deem.ru>
2017-02-26 15:18:57 +0300

    chromium 56.0.2924.110

--- build_windows/chromium-gost-publish-release.bat
+++ build_windows/chromium-gost-publish-release.bat
@@ -2,6 +2,6 @@ cd /d %~dp0
 call chromium-gost-env.bat
 
 github-release release --user deemru --repo chromium-gost --tag %CHROMIUM_TAG% --draft
-github-release upload --user deemru --repo chromium-gost --tag %CHROMIUM_TAG% --name chromium-gost-%CHROMIUM_TAG%-win32.7z --file chromium-gost-%CHROMIUM_TAG%-win32.7z
+github-release upload --user deemru --repo chromium-gost --tag %CHROMIUM_TAG% --name chromium-gost-%CHROMIUM_TAG%-win32.7z --file RELEASE\chromium-gost-%CHROMIUM_TAG%-win32.7z
 
 if "%1"=="" timeout 86400
