9b2fa1c23eaa27fcbfcc96533ed0ad04e2c554f1
Dmitrii Pichulin <deem@deem.ru>
2017-03-21 22:40:40 +0300

    fix gostssl.so linux build

--- build_linux/chromium-gost-build-gostssl.sh
+++ build_linux/chromium-gost-build-gostssl.sh
@@ -2,4 +2,7 @@
 
 cd $(dirname $0)
 . ./chromium-gost-env.sh
-g++ -Wall -Wl,--no-as-needed -std=c++11 -fPIC -shared -g -O2 -Werror -Wno-unused-function -I$BORINGSSL_PATH/ssl -I$BORINGSSL_PATH/include -I../src/msspi/third_party/cprocsp/include -I../src/msspi/src ../src/gostssl.cpp -o gostssl.so
+g++ -Wall -Wl,--no-as-needed -std=c++11 -fPIC -shared -g -O2 -Werror -Wno-unused-function -ldl \
+    -L/opt/cprocsp/lib/amd64 -lcapi10 -lcapi20 \
+    -I$BORINGSSL_PATH/ssl -I$BORINGSSL_PATH/include -I../src/msspi/third_party/cprocsp/include -I../src/msspi/src \
+    ../src/gostssl.cpp ../src/msspi/src/msspi.cpp -o gostssl.so
