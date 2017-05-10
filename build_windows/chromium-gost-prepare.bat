17d94ed6e0106fa055777cb3dc48710b4b5cd4c0
Dmitrii Pichulin <deem@deem.ru>
2017-05-10 19:05:42 +0300

    copy chromium-gost logos for win32 too

--- build_windows/chromium-gost-prepare.bat
+++ build_windows/chromium-gost-prepare.bat
@@ -16,6 +16,13 @@ call gclient sync --with_branch_heads
 call git am --3way --ignore-space-change < %CHROMIUM_GOST_REPO%\patch\chromium.patch || goto :finish
 copy /y %CHROMIUM_GOST_REPO%\extra\chromium-gost.ico chrome\app\theme\chromium\win\chromium.ico
 
+copy /y %CHROMIUM_GOST_REPO%\extra\product_logo\*.png chrome\app\theme\chromium\
+copy /y %CHROMIUM_GOST_REPO%\extra\product_logo\product_logo_16.png chrome\app\theme\default_100_percent\chromium\product_logo_16.png
+copy /y %CHROMIUM_GOST_REPO%\extra\product_logo\product_logo_32.png chrome\app\theme\default_100_percent\chromium\product_logo_32.png
+copy /y %CHROMIUM_GOST_REPO%\extra\product_logo\product_logo_32.png chrome\app\theme\default_200_percent\chromium\product_logo_16.png
+copy /y %CHROMIUM_GOST_REPO%\extra\product_logo\product_logo_64.png chrome\app\theme\default_200_percent\chromium\product_logo_32.png
+copy /y %CHROMIUM_GOST_REPO%\extra\product_logo\product_logo_32.xpm chrome\app\theme\chromium\linux\product_logo_32.xpm
+
 cd %BORINGSSL_PATH%
 call git checkout -b GOSTSSL-%CHROMIUM_TAG%
 call git checkout -f GOSTSSL-%CHROMIUM_TAG%
