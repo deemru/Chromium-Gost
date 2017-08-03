b512cad680561e4fc3939ed8702f6d88745c0006
Dmitrii Pichulin <deem@deem.ru>
2017-08-03 12:19:59 +0300

    chromium 60.0.3112.90

--- build_windows/chromium-gost-build-release.bat
+++ build_windows/chromium-gost-build-release.bat
@@ -7,6 +7,7 @@ set GYP_MSVS_VERSION=2015
 cd %CHROMIUM_PATH%
 call gn gen out\RELEASE --args="is_debug=false is_official_build=true target_cpu=\"x86\" symbol_level=0 ffmpeg_branding=\"Chrome\" proprietary_codecs=true %CHROMIUM_PRIVATE_ARGS%"
 del %CHROMIUM_PATH%\out\RELEASE\chrome.7z
+del %CHROMIUM_PATH%\out\RELEASE\*.manifest
 call ninja -C out\RELEASE mini_installer
 
 set PATH=%SEVENZIP_PATH%;%PATH%
@@ -16,7 +17,8 @@ mkdir RELEASE
 cd RELEASE
 7z x %CHROMIUM_PATH%\out\RELEASE\chrome.7z
 copy ..\gostssl.dll Chrome-bin\%CHROMIUM_TAG%
-move Chrome-bin chromium-gost-%CHROMIUM_TAG%-win32
+move Chrome-bin\chrome.exe Chrome-bin\%CHROMIUM_TAG%
+move Chrome-bin\%CHROMIUM_TAG% chromium-gost-%CHROMIUM_TAG%-win32
 7z a -mx=9 -md=256m chromium-gost-%CHROMIUM_TAG%-win32.7z chromium-gost-%CHROMIUM_TAG%-win32
 
 if "%1"=="" timeout 86400
