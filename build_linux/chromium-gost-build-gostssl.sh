09bd597c05982e0d3990e45bd3f874a703ca09a0
Dmitrii Pichulin <deem@deem.ru>
2017-10-20 12:39:09 +0300

    fix gostssl strip build

--- build_linux/chromium-gost-build-gostssl.sh
+++ build_linux/chromium-gost-build-gostssl.sh
@@ -2,7 +2,7 @@
 
 cd $(dirname $0)
 . ./chromium-gost-env.sh
-g++ -Wall -Wl,--no-as-needed -std=c++11 -fPIC -shared -g -O2 -Werror -Wno-unused-function -ldl \
+g++ -Wall -Wl,--no-as-needed -std=c++11 -fPIC -shared -s -O2 -Werror -Wno-unused-function -ldl \
     -L/opt/cprocsp/lib/amd64 -lcapi10 -lcapi20 \
     -I$BORINGSSL_PATH/ssl -I$BORINGSSL_PATH/include -I../src/msspi/third_party/cprocsp/include -I../src/msspi/src \
     ../src/gostssl.cpp ../src/msspi/src/msspi.cpp -o gostssl.so
