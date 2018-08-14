3b4e52cda861caf7bee9fb1d25e7d3d5ac325d4a
Dmitrii Pichulin <deem@deem.ru>
2018-08-14 14:28:20 +0300

    trivial fixes

--- build_mac/chromium-gost-publish-release.sh
+++ build_mac/chromium-gost-publish-release.sh
@@ -6,4 +6,4 @@ export PATH=$DEPOT_TOOLS_PATH:$PATH
 
 cd $CHROMIUM_PATH/out/RELEASE
 #github-release release --user deemru --repo chromium-gost --tag $CHROMIUM_TAG --draft
-github-release upload --user deemru --repo chromium-gost --tag $CHROMIUM_TAG --name chromium-gost-$CHROMIUM_TAG-macos-amd64.tar.bz2 --file chromium-gost-$CHROMIUM_TAG-macos-amd64.tar.bz2
+github-release upload --user deemru --repo chromium-gost --tag $CHROMIUM_TAG --name chromium-gost-$CHROMIUM_TAG-macos-amd64.tar.bz2 --file chromium-gost-$CHROMIUM_TAG-macos-amd64.signed.tar.bz2
