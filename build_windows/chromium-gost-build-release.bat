7c227e3c5840286dfa1affda225a56106bcbc04c
Dmitrii Pichulin <deem@deem.ru>
2019-04-26 17:20:49 +0300

    chromium 74.0.3729.108

--- build_windows/chromium-gost-build-release.bat
+++ build_windows/chromium-gost-build-release.bat
@@ -5,7 +5,7 @@ set DEPOT_TOOLS_WIN_TOOLCHAIN=0
 set GYP_MSVS_VERSION=2017
 
 cd %CHROMIUM_PATH%
-call gn gen out\RELEASE --args="is_debug=false symbol_level=0 strip_debug_info=true is_official_build=true ffmpeg_branding=\"Chrome\" proprietary_codecs=true %CHROMIUM_PRIVATE_ARGS% target_cpu=\"x86\""
+call gn gen out\RELEASE --args="is_debug=false is_official_build=true ffmpeg_branding=\"Chrome\" proprietary_codecs=true %CHROMIUM_PRIVATE_ARGS% target_cpu=\"x86\""
 del %CHROMIUM_PATH%\out\RELEASE\chrome.7z
 del %CHROMIUM_PATH%\out\RELEASE\*.manifest
 call ninja -C out\RELEASE mini_installer
