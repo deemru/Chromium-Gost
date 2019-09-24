2439bffcdd97c3198a2d8f33677f823ed75d050d
Dmitrii Pichulin <deem@deem.ru>
2019-09-24 09:54:37 +0300

    mitigate currently unsupported ProhibitDynamicCode

--- build_windows/chromium-gost-build-release.bat
+++ build_windows/chromium-gost-build-release.bat
@@ -5,7 +5,7 @@ set DEPOT_TOOLS_WIN_TOOLCHAIN=0
 set GYP_MSVS_VERSION=2017
 
 cd %CHROMIUM_PATH%
-call gn gen out\RELEASE --args="is_debug=false is_official_build=true ffmpeg_branding=\"Chrome\" proprietary_codecs=true %CHROMIUM_PRIVATE_ARGS% target_cpu=\"x86\""
+call gn gen out\RELEASE --args="is_debug=false is_official_build=true ffmpeg_branding=\"Chrome\" proprietary_codecs=true %CHROMIUM_PRIVATE_ARGS%"
 del %CHROMIUM_PATH%\out\RELEASE\chrome.7z
 del %CHROMIUM_PATH%\out\RELEASE\*.manifest
 call ninja -C out\RELEASE mini_installer
@@ -16,10 +16,9 @@ rmdir /s /q RELEASE
 mkdir RELEASE
 cd RELEASE
 7z x %CHROMIUM_PATH%\out\RELEASE\chrome.7z
-copy ..\gostssl.dll Chrome-bin\%CHROMIUM_TAG%
 move Chrome-bin\chrome.exe Chrome-bin\%CHROMIUM_TAG%
 move Chrome-bin\%CHROMIUM_TAG% chromium-gost-%CHROMIUM_TAG%
-7z a -mm=Deflate -mfb=258 -mpass=15 -r chromium-gost-%CHROMIUM_TAG%-windows-386.zip chromium-gost-%CHROMIUM_TAG%
+7z a -mm=Deflate -mfb=258 -mpass=15 -r chromium-gost-%CHROMIUM_TAG%-windows-amd64.zip chromium-gost-%CHROMIUM_TAG%
 :7z a -mx=9 -md=256m chromium-gost-%CHROMIUM_TAG%-windows-386.7z chromium-gost-%CHROMIUM_TAG%
 
 if "%1"=="" cmd
