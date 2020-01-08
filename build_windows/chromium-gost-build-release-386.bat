577b382a3ca5f59e87c9c33eac85823a3fa39f37
Dmitrii Pichulin <deem@deem.ru>
2020-01-08 14:57:18 +0300

    added x86 windows build

---
+++ build_windows/chromium-gost-build-release-386.bat
@@ -0,0 +1,24 @@
+cd /d %~dp0
+call chromium-gost-env.bat
+set PATH=%DEPOT_TOOLS_PATH%;%PATH%
+set DEPOT_TOOLS_WIN_TOOLCHAIN=0
+set GYP_MSVS_VERSION=2017
+
+cd %CHROMIUM_PATH%
+call gn gen out\RELEASE32 --args="is_debug=false is_official_build=true ffmpeg_branding=\"Chrome\" proprietary_codecs=true target_cpu=\"x86\" %CHROMIUM_PRIVATE_ARGS%"
+del %CHROMIUM_PATH%\out\RELEASE32\chrome.7z
+del %CHROMIUM_PATH%\out\RELEASE32\*.manifest
+call ninja -C out\RELEASE32 mini_installer
+
+set PATH=%SEVENZIP_PATH%;%PATH%
+cd %CHROMIUM_GOST_REPO%\build_windows
+rmdir /s /q RELEASE32
+mkdir RELEASE32
+cd RELEASE32
+copy %CHROMIUM_PATH%\out\RELEASE32\mini_installer.exe chromium-gost-%CHROMIUM_TAG%-windows-386-installer.exe
+7z x %CHROMIUM_PATH%\out\RELEASE32\chrome.7z
+move Chrome-bin\chrome.exe Chrome-bin\%CHROMIUM_TAG%
+move Chrome-bin\%CHROMIUM_TAG% chromium-gost-%CHROMIUM_TAG%
+7z a -mm=Deflate -mfb=258 -mpass=15 -r chromium-gost-%CHROMIUM_TAG%-windows-386.zip chromium-gost-%CHROMIUM_TAG%
+
+if "%1"=="" cmd
---
+++ build_windows/chromium-gost-publish-release-386.bat
@@ -0,0 +1,6 @@
+cd /d %~dp0
+call chromium-gost-env.bat
+
+github-release upload --user deemru --repo chromium-gost --tag %CHROMIUM_TAG% --name chromium-gost-%CHROMIUM_TAG%-windows-386-installer.exe --file RELEASE32\chromium-gost-%CHROMIUM_TAG%-windows-386-installer.exe
+
+if "%1"=="" timeout 86400
