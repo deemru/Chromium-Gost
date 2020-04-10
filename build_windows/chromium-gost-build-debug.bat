1ea4767b393ec4476025a90b1ce439d50f618cda
Dmitrii Pichulin <deem@deem.ru>
2020-04-10 14:15:27 +0300

    add windows component build support

--- build_windows/chromium-gost-build-debug.bat
+++ build_windows/chromium-gost-build-debug.bat
@@ -5,7 +5,7 @@ set DEPOT_TOOLS_WIN_TOOLCHAIN=0
 set GYP_MSVS_VERSION=2017
 
 cd %CHROMIUM_PATH%
-call gn gen out\DEBUG --args="is_debug=true is_component_build=false symbol_level=1 target_cpu=\"x86\" fatal_linker_warnings=false treat_warnings_as_errors=false %CHROMIUM_PRIVATE_ARGS%"
+call gn gen out\DEBUG --args="is_debug=true is_component_build=true symbol_level=1 target_cpu=\"x86\" fatal_linker_warnings=false treat_warnings_as_errors=false %CHROMIUM_PRIVATE_ARGS%"
 call ninja -C out\DEBUG chrome
 
 if "%1"=="" cmd
--- build_windows/chromium-gost-publish-release-386.bat
+++ build_windows/chromium-gost-publish-release-386.bat
@@ -3,4 +3,6 @@ call chromium-gost-env.bat
 
 github-release upload --user deemru --repo chromium-gost --tag %CHROMIUM_TAG% --name chromium-gost-%CHROMIUM_TAG%-windows-386-installer.exe --file RELEASE32\chromium-gost-%CHROMIUM_TAG%-windows-386-installer.exe
 
+github-release upload --user deemru --repo chromium-gost --tag %CHROMIUM_TAG% --name chromium-gost-%CHROMIUM_TAG%-windows-386.zip --file RELEASE32\chromium-gost-%CHROMIUM_TAG%-windows-386.zip
+
 if "%1"=="" timeout 86400
