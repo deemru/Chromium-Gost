672502680f84bff2bdd79abdbb5e2658cf3a952b
Dmitrii Pichulin <deem@deem.ru>
2022-03-15 15:29:20 +0300

    add create patch script

---
+++ build_windows/chromium-gost-patch.bat
@@ -0,0 +1,15 @@
+cd /d %~dp0
+call chromium-gost-env.bat
+set PATH=%DEPOT_TOOLS_PATH%;%PATH%
+set DEPOT_TOOLS_WIN_TOOLCHAIN=0
+set GOST_BRANCH=GOSTSSL-%CHROMIUM_TAG%
+
+cd %CHROMIUM_PATH%\.git || goto :finish
+cd %BORINGSSL_PATH%\.git || goto :finish
+
+cd %CHROMIUM_PATH%
+cmd /C "git format-patch HEAD~1 --stdout > %CHROMIUM_GOST_REPO%patch\chromium.patch"
+cd %BORINGSSL_PATH%
+cmd /C "git format-patch HEAD~1 --stdout > %CHROMIUM_GOST_REPO%patch\boringssl.patch"
+
+cmd
