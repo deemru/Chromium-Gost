9def9c760e1f2a665bf034f5eb518e181eb28b3b
Dmitrii Pichulin <deem@deem.ru>
2023-04-05 22:25:57 +0300

    cleanup unused env

--- build_windows/chromium-gost-env.bat
+++ build_windows/chromium-gost-env.bat
@@ -8,5 +8,4 @@ set SEVENZIP_PATH="C:\Program Files\7-Zip\"
 set CHROMIUM_PRIVATE_ARGS= 
 if exist chromium-gost-env-private.bat call chromium-gost-env-private.bat
 if exist %USERPROFILE%\chromium-gost-env-private.bat call %USERPROFILE%\chromium-gost-env-private.bat
-set GYP_MSVS_VERSION=2019
 set PATH=%DEPOT_TOOLS_PATH%;%DEPOT_TOOLS_PATH%\python-bin;%PATH%
