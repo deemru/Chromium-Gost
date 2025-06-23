39fbd2d0506d0d6e37551557fd23af3f0ded8c19
Dmitrii Pichulin <deem@deem.ru>
2025-06-23 14:50:45 +0300

    adopt patches

--- build_windows/chromium-gost-patch.bat
+++ build_windows/chromium-gost-patch.bat
@@ -8,11 +8,13 @@ cd %CHROMIUM_PATH%\.git || goto :finish
 cd %BORINGSSL_PATH%\.git || goto :finish
 
 cd %CHROMIUM_PATH%
-cmd /C "git format-patch HEAD~1 --stdout > %CHROMIUM_GOST_REPO%patch\chromium.patch"
+cmd /C "git format-patch HEAD~1 --stdout > %CHROMIUM_GOST_REPO%patch\extra\extensions-manifestv2_ifdef.stage.patch"
+cd %CHROMIUM_PATH%
+cmd /C "git format-patch HEAD~2..HEAD~1 --stdout > %CHROMIUM_GOST_REPO%patch\chromium.stage.patch"
 cd %BORINGSSL_PATH%
-cmd /C "git format-patch HEAD~1 --stdout > %CHROMIUM_GOST_REPO%patch\boringssl.patch"
+cmd /C "git format-patch HEAD~1 --stdout > %CHROMIUM_GOST_REPO%patch\boringssl.stage.patch"
 cd %CHROMIUM_PATH%\third_party\search_engines_data\resources
-cmd /C "git format-patch HEAD~1 --stdout > %CHROMIUM_GOST_REPO%patch\search_engines_data.patch"
+cmd /C "git format-patch HEAD~1 --stdout > %CHROMIUM_GOST_REPO%patch\search_engines_data.stage.patch"
 
 curl -o %CHROMIUM_GOST_REPO%patch\extra\extensions-manifestv2.patch https://raw.githubusercontent.com/ungoogled-software/ungoogled-chromium/refs/heads/master/patches/core/ungoogled-chromium/extensions-manifestv2.patch
 
